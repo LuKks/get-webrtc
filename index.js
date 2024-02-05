4171e2796be08e057ac0d28afbd264f1ea36c61e
Lucas Barrena <lucasbarrena98@gmail.com>
2024-02-04 23:23:30 -0300

    Fix the patch leak until PR merge

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ module.exports = require('werift')
 // Temporal patch until leak fix gets merged
 // https://github.com/shinyoshiaki/werift-webrtc/pull/365
 
-const { SCTP } = require('./node_modules/werift/lib/sctp/src/sctp.js')
+const path = require('path')
+const { SCTP } = requireModule()
 
 const stop = SCTP.prototype.stop
 const setState = SCTP.prototype.setState
@@ -18,4 +19,14 @@ SCTP.prototype.setState = function (state) {
   setState.call(this, state)
 
   if (state === 1) this.timerReconfigCancel()
+}
+
+function requireModule () {
+  try {
+    const filename = path.join(__dirname, 'node_modules', 'werift', 'lib', 'sctp', 'src', 'sctp.js')
+    return require(filename)
+  } catch {
+    const filename = path.join(__dirname, '..', 'werift', 'lib', 'sctp', 'src', 'sctp.js')
+    return require(filename)
+  }
 }
